12216990d526bac67555cafcea865754f8ceda07
DenVitaKaninen <tobiaskjernell@gmail.com>
2025-09-03 17:55:34 +0200

    added tanstackQuery and worked out the fundamentals

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,18 +1,18 @@
-import { useEffect } from "react";
+import CabinTable from "../features/cabins/CabinTable";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
-import { getCabins } from "../services/apiCabins";
 
 function Cabins() {
-  useEffect(() => {
-    getCabins().then(data => console.log(data))
-  }, []) ///Tmp check
-
   return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
-    </Row>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">All cabins</Heading>
+        <div>Filter / Sort</div>
+      </Row>
+      <Row>
+        <CabinTable />
+      </Row>
+    </>
   );
 }
 
